0c2ddfa511718d4be33d51238ca83a4d717b9ae3
Prasad12323 <Babu.Vara.Prasad.Nujella@ibm.com>
2026-06-12 09:06:10 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,16 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c1 = new Car("Black", "Manual", 2014);
+		
+		// Use the object to call its functions
+		c1.displayCharacteristics();
+		c1.accelarate();
+		c1.brake();
+
+	}
+
+}
